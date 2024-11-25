ac881536fad58d13db4caddd7b5f6d046704c470
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-11-24 19:26:07 -0500

    2-15

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ali Elouasbi
+ * Created on: Nov 2024
+ * This program makes pixels move around edges
 */
+ //Variables
+let sprite: game.LedSprite = null
 
-basic.showString('Hello, World!')
+input.onButtonPressed(Button.A, function () {
+    sprite = game.createSprite(0, 0)
+    sprite.set(LedSpriteProperty.Direction, 0)
+
+    let loopCounter = 0
+    while (loopCounter < 5) { 
+        let stepCounter = 0
+
+        while (stepCounter < 5) {
+            sprite.move(1)
+            basic.pause(500)
+            stepCounter = stepCounter + 1
+        }
+
+        // Turn the sprite to face the next edge
+        sprite.turn(Direction.Right, 90)
+        loopCounter = loopCounter + 1
+    }
+    // Delete the sprite when done
+    sprite.delete()
+})
